b7ef628c45073552a03f297165a943eb4898b205
Kundan-S11 <16kundansharma@gmail.com>
2026-07-17 19:26:42 +0530

    percentage
    my percentage calculation through codes

--- codes.py
+++ codes.py
@@ -1,1 +1,11 @@
 print("HELLO WORLD")
+print('Percentage')
+mathematics=71
+chemistry=71
+english=75
+ip=87
+physics=67
+total=(mathematics+chemistry+english+ip+physics)
+Percentage=total%500
+print(Percentage)
+
